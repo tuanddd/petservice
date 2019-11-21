61e7deaa3517139e15de9d39c7cd3cf7e19a2cdd
Đào Tuấn <heryhudson@gmail.com>
2019-11-21 23:01:14 +0700

    default name to user

--- src/router/user.ts
+++ src/router/user.ts
@@ -8,7 +8,7 @@ export default class UserRouter extends CrudRouter<User, typeof User> {
     super(User);
 
     this.router.post(`/custom/register-user-firebase`, async (req, res) => {
-      let { userId, name, email } = req.body;
+      let { userId, name = 'user', email } = req.body;
       res.status(200).json(await this.service.createUserWithProvider('firebase', userId, { email, name, petDataJson: '', roleId: 3 }))
     })
   }
